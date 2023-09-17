11ec509b83e72d8f42864d6cb48948c269da58e2
student <student@PC-G-05>
2023-09-17 11:12:20 +0300

    lab4

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 num1 = float(input("Введіть перше число:"))
 num2 = float(input("Введіть друге число:"))
 
-maximum = max(num1, num2)
-print(f"Більшим числом є {maximum}")
+minimum = min(num1, num2)
+print(f"Меншим числом є {minimum}")
