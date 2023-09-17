b6844e4ef093daa2040a96516f688fa55855c860
student <student@PC-G-05>
2023-09-17 11:03:16 +0300

    lab2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-number = int(input("Введіть число: "))
+number = int(input("Введіть число, кратне 7:"))
 
-if number % 2 ==0:
-    print(f"{number} - Even number")
+if number % 7 == 0:
+    print(f"{number} - Number is a multiple of 7")
 else:
-    print(f"{number} - Odd number")
+    print(f"{number} - Number is not multiple of 7")
